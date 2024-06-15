4293f6aa2b231d85187bb95fbd188a54460923d8
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-15 16:43:43 -0500

    "add basic file structure w/ beginning feature files"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './components/App/App';
+import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
