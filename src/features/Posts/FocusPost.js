4c8e38c86ec25b7dd1f1a572b170d858b8d6992b
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-19 09:16:01 -0500

    "basic functionality to view a single post, only links to first mock post"

--- src/features/Posts/FocusPost.js
+++ src/features/Posts/FocusPost.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Post from "./Post";
 import Comments from "../Comments/Comments";
+import mockdata from "../../Util/mock";
 
 const Focus = () => {
+    const postData = mockdata.data.children[0];
+
     return (
         <div>
-            <Post focus={true}/>
+            <Post focus={true} post={postData.data}/>
             <Comments />
         </div>
     )
--- src/features/Posts/Post.js
+++ src/features/Posts/Post.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Post = ({ post }) => {
+const Post = ({ post, focus }) => {
     return (
         <div className="post">
             <p>{post.subreddit_name_prefixed}</p>
             <p>{post.author}</p>
-            <h3>{post.title}</h3>
-            <img src={post.thumbnail}/>
+            <Link to={focus ? null : `/${post.id}`}>
+                <h3>{post.title}</h3>
+                <img src={post.thumbnail}/>
+            </Link>
             <p>{post.ups}</p>
             <p>{post.num_comments}</p>
         </div>
