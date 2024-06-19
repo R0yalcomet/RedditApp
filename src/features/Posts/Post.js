232885f6937239580a48ad6fdf46813991e28c14
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-19 14:59:12 -0500

    "Post component shows more info in focus view than on feed view"

--- src/features/Posts/Post.js
+++ src/features/Posts/Post.js
@@ -2,14 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Post = ({ post, focus }) => {
+    const main = () => {
+        if (focus) {
+            return (
+                <div>
+                    <h3>{post.title}</h3>
+                    <h4>{post.selftext}</h4>
+                    <img src={post.thumbnail}/>
+                </div>
+            )
+        } else {
+            return (
+                <Link to={`/${post.id}`}>
+                    <h3>{post.title}</h3>
+                    <img src={post.thumbnail}/>
+                </Link>
+            )
+        }
+    }
+
     return (
         <div className="post">
             <p>{post.subreddit_name_prefixed}</p>
             <p>{post.author}</p>
-            <Link to={focus ? null : `/${post.id}`}>
-                <h3>{post.title}</h3>
-                <img src={post.thumbnail}/>
-            </Link>
+            {main()}
             <p>{post.ups}</p>
             <p>{post.num_comments}</p>
         </div>
