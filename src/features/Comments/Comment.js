2550519be54d6e140fbfacdc44c1d2060fd153b0
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-19 15:57:55 -0500

    "top level comments now show, replies still WIP"

--- src/features/Comments/Comment.js
+++ src/features/Comments/Comment.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Comment = () => {
+const Comment = ({ commentData }) => {
     return (
         <div>
-
+            <p>{commentData.body}</p>
         </div>
     )
 };
--- src/features/Comments/Comments.js
+++ src/features/Comments/Comments.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Comment from "./Comment";
+import { mockComments } from "../../Util/mock";
 
 const Comments = () => {
+    const commentsData = mockComments[1].data.children;
+
     return (
         <div>
-            
+            {commentsData.map((comment) => <Comment commentData={comment.data}/>)}
         </div>
     )
 };
