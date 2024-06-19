2434ce44a4d9c1ce96cf6d15421e362baa6f0dac
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-19 08:58:38 -0500

    "setup Comments/Comment components, no functionality yet"

---
+++ src/features/Comments/Comments.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+const Comments = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default Comments;
