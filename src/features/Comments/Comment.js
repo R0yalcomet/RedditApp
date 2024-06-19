e2aa31f0c9b5920c666ec705590d35e9a7dedb49
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-19 16:37:13 -0500

    "placeholder styles for Comments/Comment components"

--- src/features/Comments/Comment.js
+++ src/features/Comments/Comment.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Comment = ({ commentData }) => {
     return (
-        <div>
+        <div className="comment">
+            <p>{commentData.author}</p>
+            <p>{commentData.depth}</p>
             <p>{commentData.body}</p>
         </div>
     )
