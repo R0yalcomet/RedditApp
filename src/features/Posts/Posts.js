7be50b504289ba7517d749e9e655d350972bf1c0
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-20 20:45:54 -0500

    "add loading condition"

--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -4,16 +4,21 @@ import Post from "./Post";
 
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadFeed, selectAllPosts } from "./PostsSlice";
+import { loadFeed, selectAllPosts, isLoadingFeed } from "./PostsSlice";
 
 
 const Posts = () => {
     const dispatch = useDispatch();
     const posts = useSelector(selectAllPosts);
+    const isLoading = useSelector(isLoadingFeed);
 
     useEffect(() => {
         dispatch(loadFeed());
     }, [dispatch]);
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div className="posts">
