c7a1b4cad917253e5e9a475ca81546356c16fb65
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-25 12:27:51 -0500

    "cleanup and notation"

--- src/features/Search/SubReddit.js
+++ src/features/Search/SubReddit.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+
 import { setFilterSubreddit, loadFeed } from "../Posts/PostsSlice";
 
 const Subreddit = ({ subName }) => {
     const dispatch = useDispatch();
     
-    const handleClick = () => {
+    const handleClick = () => { //reload feed with posts from chosen subreddit
         dispatch(setFilterSubreddit(subName));
         dispatch(loadFeed());
     }
+
+    //ToDo:
+    //clearly display which subreddit is selected
+    //add option to clear subreddit filter
 
     return (
         <li onClick={handleClick} value={subName}>{subName}</li>
