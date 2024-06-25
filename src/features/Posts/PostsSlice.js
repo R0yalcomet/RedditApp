17fb5a99c6563b890aa3f6cf135c6256329cbab4
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-06-25 12:14:41 -0500

    "notation and cleanup"

--- src/features/Posts/PostsSlice.js
+++ src/features/Posts/PostsSlice.js
@@ -1,39 +1,38 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import assembleUrl from "../../API/reddit";
 
-// const url = 'https://www.reddit.com/';
-
-export const loadFeed = createAsyncThunk(
+export const loadFeed = createAsyncThunk( //load list of posts
     'posts/loadFeed',
     async (search, store) => {        
-        const subName = store.getState().posts.filterSubreddit;
-
-        const searchParams = {
+        const subName = store.getState().posts.filterSubreddit; //grab subreddit filter from state
+        const searchParams = { //store search term and subreddit in object to pass onto assembleUrl function
             searchTerm: search,
             subName: subName
-        }
+        };
         const url = assembleUrl(searchParams); 
+
         const response = await fetch(url);
         if (!response.ok) {
             const error = await response.json()
             const message = `An error has occurred: ${response.status} ${error.message}`;
             throw new Error(message);
-        }
+        };
         const data = await response.json();
         return data;
     }
 );
 
-export const loadPost = createAsyncThunk(
+export const loadPost = createAsyncThunk( //load a single post in focus view
     'posts/loadPost',
     async (postLink) => {
         const url = 'https://www.reddit.com/';
+
         const response = await fetch(url + postLink + '.json');
         if (!response.ok) {
             const error = await response.json()
             const message = `An error has occurred: ${response.status} ${error.message}`;
             throw new Error(message);
-        }
+        };
         const data = await response.json();
         return data;
     }
@@ -48,7 +47,7 @@ const postsSlice = createSlice({
         fetchFeedFailed: false,
         loadingFocus: false,
         fetchFocusFailed: false,
-        filterSubreddit: ''
+        filterSubreddit: '' 
     },
     reducers: {
         setFilterSubreddit: (state, action) => {
@@ -78,6 +77,10 @@ const postsSlice = createSlice({
                 state.loadingFocus = false;
                 state.fetchFocusFailed = false;
                 state.focusState = action.payload; //payload contains both the post and its comments
+            })
+            .addCase(loadPost.rejected, (state) => {
+                state.loadingFocus = false;
+                state.fetchFocusFailed = true;
             })
     }
 });
