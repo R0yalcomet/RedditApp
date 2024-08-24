b21c385198b4fa1befea9901c6786b8075bc0441
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-08-24 12:15:42 -0500

    "clean up Comment code"

--- src/features/Comments/Comments.js
+++ src/features/Comments/Comments.js
@@ -3,22 +3,13 @@ import Comment from "./Comment";
 
 const Comments = ({ comments }) => {
     const ListComments = (comment) => {
-        //iterates through replies until all comments are shown
-        if (comment.data.replies) {
-            //return comment and input replies back into this function
-            const replies = comment.data.replies.data.children;
-            return (
-                <>
-                    {!comment.data.body ? null : <Comment key={comment.data.id} commentData={comment.data}/>}
-                    {replies.map(reply => ListComments(reply))}
-                </>
-            )
-        } else {
-            //return a comment with no replies
-            return (
-                !comment.data.body ? null : <Comment key={comment.data.id} commentData={comment.data}/>
-            ) 
-        }
+        //recursively check for and display comments/replies
+        return (
+            <>
+                {!comment.data.body ? null : <Comment key={comment.data.id} commentData={comment.data}/>}
+                {!comment.data.replies ? null : comment.data.replies.data.children.map(reply => ListComments(reply))}
+            </>
+        )
     };
 
     return (
